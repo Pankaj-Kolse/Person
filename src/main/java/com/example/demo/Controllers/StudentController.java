94ec2c858f8e8bb0c9f3ecf69312af6eba7e7139
Pankaj <kolsepankaj6@gmail.com>
2023-08-03 12:37:36 +0530

    Person Commit

--- src/main/java/com/example/demo/Controllers/StudentController.java
+++ src/main/java/com/example/demo/Controllers/StudentController.java
@@ -5,15 +5,10 @@ import com.example.demo.entities.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.example.demo.services.StudentService;
-
+@CrossOrigin("*")
+@RequestMapping("/Person")
 @RestController
 public class StudentController {
 
--- src/main/java/com/example/demo/PersonApplication.java
+++ src/main/java/com/example/demo/PersonApplication.java
@@ -20,3 +20,4 @@ public class PersonApplication extends SpringBootServletInitializer {
 
 }
 
+
